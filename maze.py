05b87154b606a54e3c2f22940b36c2c6f51f8410
Edwin <edwinluisdiaz@gmail.com>
2023-03-23 22:19:14 -0400

    Maze*

---
+++ maze.py
@@ -0,0 +1,17 @@
+POS_X = 0
+POS_Y = 1
+MAP_WIDTH = 20
+MAP_HEIGHT = 15
+
+my_position = [19, 14]
+
+print("+" + "-" * MAP_WIDTH * 3 + "+")
+for cordinate_y in range(MAP_HEIGHT):
+    print("|", end="")
+    for cordinate_x in range(MAP_WIDTH):
+        if my_position[POS_X] == cordinate_x and my_position[POS_Y] == cordinate_y:
+            print(" @ ", end="")
+        else:
+            print("   ", end="")
+    print("|")
+print("+" + "-" * MAP_WIDTH * 3 + "+")
