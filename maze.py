baa261bbb499879c970390f50952cadbb86bc047
Edwin <edwinluisdiaz@gmail.com>
2023-04-03 00:08:19 -0400

    Putting cebos 1

--- maze.py
+++ maze.py
@@ -6,24 +6,26 @@ POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
 my_position = [10, 6]
+map_objects = [[2, 3], [5, 4], [3, 4], [10, 6]]
 
 while True:
-
     print("+" + "-" * MAP_WIDTH * 3 + "+")
     for cordinate_y in range(MAP_HEIGHT):
         print("|", end="")
         for cordinate_x in range(MAP_WIDTH):
+            char_to_draw = " "
+            for map_object in map_objects:
+                if map_object[POS_X] == cordinate_x and map_object[POS_Y] == cordinate_y:
+                    char_to_draw = "*"
             if my_position[POS_X] == cordinate_x and my_position[POS_Y] == cordinate_y:
-                print(" @ ", end="")
-            else:
-                print("   ", end="")
+                char_to_draw = "@"
+            print(f" {char_to_draw} ", end="")
         print("|")
     print("+" + "-" * MAP_WIDTH * 3 + "+")
     print("\nprecione [q] para salir")
 
     direction = readchar.readchar()
 
-    
     if direction in ["w", "W"]:
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
