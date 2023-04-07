a6d2ac71c1b8be1ae4bc632cb052a7c22e9cd93b
Edwin <edwinluisdiaz@gmail.com>
2023-04-07 19:27:52 -0400

    Finish snake game

--- maze.py
+++ maze.py
@@ -1,14 +1,35 @@
 import readchar
 from os import system
+from random import randint
+from time import sleep
 
+#Global variables
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
-my_position = [10, 7]
-map_objects = [[2, 3], [5, 4], [3, 4], [7, 8], [9, 10], [5, 9]]
+NUM_MAP_OBJECTS = 11
 
+my_position = [10, 7]
+tail_length = 0
+map_objects = []
+tail = []
+
+#Generating objects 
+while len(map_objects) < NUM_MAP_OBJECTS:
+    new_position = [randint(0, MAP_WIDTH), randint(0, MAP_HEIGHT)]
+
+    if new_position not in map_objects and my_position != new_position:
+        map_objects.append(new_position)
+    
+#Main loop
 while True:
+    if my_position in tail:
+        system('cls')
+        print('You losse')
+        sleep(2)
+        break
+    #Start printing the map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
     for cordinate_y in range(MAP_HEIGHT):
         print("|", end="")
@@ -19,27 +40,48 @@ while True:
                 if map_object[POS_X] == cordinate_x and map_object[POS_Y] == cordinate_y:
                     char_to_draw = "*"
                     object_in_cell = map_object
+            for tail_piece in tail:
+                if tail_piece[POS_X] == cordinate_x and tail_piece[POS_Y] == cordinate_y:
+                    char_to_draw = "@"
             if my_position[POS_X] == cordinate_x and my_position[POS_Y] == cordinate_y:
                 char_to_draw = "@"
                 if object_in_cell:
                     map_objects.remove(object_in_cell)
+                    tail_length += 1
             print(f" {char_to_draw} ", end="")
         print("|")
+    #Finish printing the map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
-    print("\nprecione [q] para salir")
+    #Q for quit the game
+    print("\nPress [q] for quit")
+
+    if len(map_objects) < 1:
+        system('cls')
+        print('You win')
+        sleep(2)
+        break
 
     direction = readchar.readchar()
 
+    #Directions keys
     if direction in ["w", "W"]:
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction in ["s", "S"]:
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] += 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction in ["a", "A"]:
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] -= 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction in ["d", "D"]:
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction in ["q", "Q"]:
