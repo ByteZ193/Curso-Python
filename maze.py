9ffdfb747d530b360e4c08fdb3133dc5872827ac
Edwin <edwinluisdiaz@gmail.com>
2023-04-07 16:35:55 -0400

    Fixing desapearing objects

--- maze.py
+++ maze.py
@@ -5,8 +5,8 @@ POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
-my_position = [10, 6]
-map_objects = [[2, 3], [5, 4], [3, 4], [10, 6]]
+my_position = [10, 7]
+map_objects = [[2, 3], [5, 4], [3, 4], [7, 8], [9, 10], [5, 9]]
 
 while True:
     print("+" + "-" * MAP_WIDTH * 3 + "+")
@@ -14,11 +14,15 @@ while True:
         print("|", end="")
         for cordinate_x in range(MAP_WIDTH):
             char_to_draw = " "
+            object_in_cell = None
             for map_object in map_objects:
                 if map_object[POS_X] == cordinate_x and map_object[POS_Y] == cordinate_y:
                     char_to_draw = "*"
-            if my_position[POS_X] == cordinate_x and my_position[POS_Y] == cordinate_y and my_position not in map_objects:
+                    object_in_cell = map_object
+            if my_position[POS_X] == cordinate_x and my_position[POS_Y] == cordinate_y:
                 char_to_draw = "@"
+                if object_in_cell:
+                    map_objects.remove(object_in_cell)
             print(f" {char_to_draw} ", end="")
         print("|")
     print("+" + "-" * MAP_WIDTH * 3 + "+")
