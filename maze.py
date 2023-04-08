e2d736a006ccf4a5c522d74bb38380fc541a3b27
Edwin <edwinluisdiaz@gmail.com>
2023-04-07 20:52:05 -0400

    Snake complete

--- maze.py
+++ maze.py
@@ -14,10 +14,12 @@ my_position = [10, 7]
 tail_length = 0
 map_objects = []
 tail = []
+new_position = []
+object_in_cell = []
 
 #Generating objects 
 while len(map_objects) < NUM_MAP_OBJECTS:
-    new_position = [randint(0, MAP_WIDTH), randint(0, MAP_HEIGHT)]
+    new_position = [randint(0, MAP_WIDTH - 1), randint(0, MAP_HEIGHT - 1)]
 
     if new_position not in map_objects and my_position != new_position:
         map_objects.append(new_position)
@@ -48,18 +50,17 @@ while True:
                 if object_in_cell:
                     map_objects.remove(object_in_cell)
                     tail_length += 1
+            if len(map_objects) < 1:
+                new_position = [randint(0, MAP_WIDTH - 1), randint(0, MAP_HEIGHT - 1)]
+                if new_position not in map_objects and my_position != new_position and new_position not in tail:
+                    map_objects.append(new_position)
             print(f" {char_to_draw} ", end="")
         print("|")
     #Finish printing the map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print(map_objects)
     #Q for quit the game
     print("\nPress [q] for quit")
-
-    if len(map_objects) < 1:
-        system('cls')
-        print('You win')
-        sleep(2)
-        break
 
     direction = readchar.readchar()
 
@@ -85,7 +86,6 @@ while True:
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction in ["q", "Q"]:
-        break
+         break
     system('cls')
-
 system('cls')
