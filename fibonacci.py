323d01ca08687823428abedfef0da12cd8e1f597
Edwin <edwinluisdiaz@gmail.com>
2023-06-04 23:45:03 -0400

    Iniciando con funciones

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+def fibonacci1(n):
+    if n < 2:
+        return n
+    return fibonacci1(n - 2) + fibonacci1(n - 1)
+
+
+def fibonacci2(n):
+    fib = 0
+    n1, n2 = 0, 1
+    i = 0
+    if n < 2:
+        return n
+    while i < n:
+        print(n1)
+        fib = n1 + n2
+        n1 = n2
+        n2 = fib
+        i += 1
+    return n1
+
+
+if __name__ == '__main__':
+    print(fibonacci1(6))
+    print(fibonacci2(4))
---
+++ funciones2.py
@@ -0,0 +1,8 @@
+def square(num):
+    number_square = num * num
+    return number_square
+
+
+if __name__ == '__main__':
+    num = int(input("Enter a number:\n"))
+    print(square(num))
