9cde41458ed6985a769b3fdae7eec7d9bb9fb310
Edwin <edwinluisdiaz@gmail.com>
2023-06-11 17:18:11 -0400

    Boring class #2

--- boringclass.py
+++ boringclass.py
@@ -1,4 +1,6 @@
 from os import system
+from string import ascii_uppercase
+from string import ascii_lowercase
 
 #Ejejcicio 1: Crea una funcion que reciba una lista de strings como entrada y te diga cual es la más larga de todas
 def most_long_string(*args):
@@ -34,11 +36,34 @@ def question():
             return False
     return True
 
+#Ejercicio 5: Crea una función que convierta toda una string en mayusculas, no vale usar el método upper()
+def upper(string):
+    result = ""
+    for s in string:
+        if(ord(s) >= 97):
+            result += chr(ord(s) - 32)
+        else:
+            result += s
+    return print(result)
+
+# Ejercicio 6: Crea una función que reciba como argumento un número del 1 al 100 a adivinar y que le pregunte al usuario
+# que adivine el número. El código se ejecutará hasta que el usuario adivine.
+def guess(n):
+    guess = input("Adivine el numero 1 al 100, favor introducir solo numeros:\n")
+    while guess in ascii_uppercase or guess in ascii_lowercase or guess != str(n):
+        system('cls')
+        guess = input("No adivinaste!!, vuelve a intentarlo; favor introducir solo numeros del 1 al 100:\n")
+    system('cls')
+    return print("Adivinaste!!!")
+
 def main():
+
     #most_long_string('hola', 'esto no tiene sentido', 'adios', 'Supercalifragilisticoespialidoso')
     #plus(2, 2)
     #print(odd(2))
-    print(question())
+    #print(question())
+    #upper("Hola klk Edwin")
+    #guess(3)
 
 if __name__ == '__main__':
     main()
