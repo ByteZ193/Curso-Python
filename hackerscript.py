81cded19525db8f82ffae498f3105b4a85a51531
ByteZ193 <edwinluisdiaz@gmail.com>
2023-12-19 12:02:53 -0400

    Agregando comentarios

--- hackerscript.py
+++ hackerscript.py
@@ -1,17 +1,22 @@
+#Script para asustar a tus amigos creando un archivo con el mensaje de un hacker, tambien para chequear 
+#Importando librerias
+
 import os
 import sqlite3
 from time import sleep
 from random import randrange
 
-
+#Creando el archivo donde se guardara la información del script
 HACKER_FILE_NAME = "PARA TI.txt"
 
+#Funcion para crear el archivo
 def create_hacker_file(desktop_path):
     hacker_file = open(desktop_path + HACKER_FILE_NAME, "w")
     hacker_file.write("Soy un hacker y he entrado en tu sistema")
     hacker_file.close()
     return hacker_file
 
+#Funcion para obtener el historial de Chrome
 def get_chrome_history(history_path):
     try:
         con = sqlite3.connect(history_path)
@@ -23,6 +28,7 @@ def get_chrome_history(history_path):
     except sqlite3.OperationalError as e:
         return None
 
+#Funcion para aplicar un tiempo entre accionses
 def delay_action():
     n_houers = randrange(1, 4)
     print(f"Esperando {n_houers} horas")
